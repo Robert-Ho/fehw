b6eef08a39466957a7cbbf4c0677529a4300bddb
Robert-Ho <66453666+Robert-Ho@users.noreply.github.com>
2023-07-14 11:05:45 +0700

    Update test_ui_makino.py
    update screen size and flow for tabble

--- test_ui_makino.py
+++ test_ui_makino.py
@@ -4,20 +4,27 @@ from sbvt.visualtest import VisualTest
 
 class Makino(BaseCase):
         
-    def test_homepage(self):
+   def test_homepage(self):
         settings = {'projectToken': "N9e7XGvw/vZJYtpKZiLQ="}
         visualTest = VisualTest(self.driver, settings)
         url = "https://www.makino.com/"
         self.open(url)
-        self.maximize_window()
+        self.set_window_size(1024,693)
         print(self.get_page_title())
         
         visualTest.capture("Home_Page",{'lazyload': 1000})
 
+        self.click('button.dl-trigger')
+        self.sleep(1)
         self.click_link('Digital Makino')
+        self.click_link('Overview')
+       
         visualTest.capture("Digital_Makino_page",{'lazyload': 1000})
-
+        
+        self.click('button.dl-trigger')
+        self.sleep(1)
         self.click_link("Machine Technology")
+        self.click_link('Overview')
         visualTest.capture("Machine_Technology_page",{'lazyload': 1000})
 
         
